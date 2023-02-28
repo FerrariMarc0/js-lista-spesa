77702585b677c3c15ea724441cd6ebca4aa76272
FerrariMarc0 <marco1010@live.it>
2023-02-28 15:30:51 +0100

    while/do-while

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 'use strict';
 
+const marketList= [
+    'vino',
+    'prosecco',
+    'lambrusco',
+    'salatini',
+    'mascarpone',
+    'salsiccia',
+    'pane',
+    'pannolini per Rinaldo',
+    'scottex'
+];
+
+marketList.push('pappa lattea', 'omogeneizzati');
+
+const products= document.getElementById('list');
+let i= 0;
+let x= marketList.length;
+
+while(i < x){
+    
+    const listItem= document.createElement('li');
+    listItem.append(marketList[i]);
+    products.append(listItem);
+    i++
+}
+console.log(marketList);
 
 
+
+/*do{
+    const listItem= document.createElement('li');
+    listItem.append(marketList[i]);
+    products.append(listItem);
+    i++
+} while(i < x);
+
+console.log(marketList);*/
